af562cbb6bd9c7e330ec3a701ff5df120677b569
Mohammed Ajmal Siddiqui <ajmalsiddiqui21@gmail.com>
2017-09-02 13:55:50 +0530

    secrets added

--- users/mailer.js
+++ users/mailer.js
@@ -1,40 +1,45 @@
 const nodeMailer = require('nodemailer');
+const fs = require('fs');
 
 const config = require('../config');
 const User = require('./user');
 const generateQr = require('./generate-qr');
 
-const transport = nodeMailer.createTransport({
-  service: config.mailService,
-  auth: {
-    user: require('../secrets').mailId || process.env.MAIL_ID,
-    pass: require('../secrets').mailPass || process.env.MAIL_PASS
-  }
-});
-
 const sendMail = function(userEmail, callback){
 
-  let mailOptions = {
-    from: config.from,
-    subject: config.subject,
-    text: config.text,
-    to: userEmail
-  };
+  fs.access('../secrets.js', err => {
+    const secrets = err ? 0 : 1;
 
-  User.findOne({email: userEmail}, (err, user) => {
-    if(err) return callback(err);
-
-    //returns a stream with png of qr code
-    mailOptions.attachments = [
-      {
-        filename: config.attachment_name,
-        content: generateQr.generateQr(user)
+    const transport = nodeMailer.createTransport({
+      service: config.mailService,
+      auth: {
+        user: secrets ? require('../secrets').mailId : process.env.MAIL_ID,
+        pass: secrets ? require('../secrets').mailPass : process.env.MAIL_PASS
       }
-    ];
+    });
 
-    transport.sendMail(mailOptions, (err, info) => {
+    let mailOptions = {
+      from: config.from,
+      subject: config.subject,
+      text: config.text,
+      to: userEmail
+    };
+
+    User.findOne({email: userEmail}, (err, user) => {
       if(err) return callback(err);
-      callback(null, info);
+
+      //returns a stream with png of qr code
+      mailOptions.attachments = [
+        {
+          filename: config.attachment_name,
+          content: generateQr.generateQr(user)
+        }
+      ];
+
+      transport.sendMail(mailOptions, (err, info) => {
+        if(err) return callback(err);
+        callback(null, info);
+      });
     });
   });
 }
--- users/signup.js
+++ users/signup.js
@@ -4,23 +4,25 @@ const User = require('./user');
 const mailer = require('./mailer');
 const config = require('../config');
 
-const db_url = require('../secrets').db_url || process.env.DB_URL;
-
-mongoose.connect(db_url);
-
 const signup = function(email, name, mobile, regno, callback){
-  const newUser = new User({
-    email: email,
-    name: name,
-    mobile: mobile,
-    regno: regno
-  });
-  console.log(newUser);
-  newUser.save(err => {
-    //console.log('trying to save');
-    if(err) return callback(err);
-    //console.log('user saved');
-    mailer.sendMail(email, callback);
+  fs.access('../secrets', err => {
+    const db_url = !err ? require('../secrets').db_url : process.env.DB_URL;
+
+    mongoose.connect(db_url);
+
+    const newUser = new User({
+      email: email,
+      name: name,
+      mobile: mobile,
+      regno: regno
+    });
+    console.log(newUser);
+    newUser.save(err => {
+      //console.log('trying to save');
+      if(err) return callback(err);
+      //console.log('user saved');
+      mailer.sendMail(email, callback);
+    });
   });
 }
 
