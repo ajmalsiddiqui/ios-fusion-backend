46e9bbd1492b466dd2dbefb26b8964839c3a5a2d
Mohammed Ajmal Siddiqui <ajmalsiddiqui21@gmail.com>
2017-09-02 14:11:48 +0530

    secrets added

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ config.subject = 'Ticket to iOS Fusion 3.0';
 config.text = 'Hello!\nYou have been successfully registered in iOS Fusion 3.0. Your ticket is attached with the mail. Kindly carry it when you come for the event.\n\nRegards,\nTeam iOS Fusion';
 config.attachment_name = 'ticket.png';
 
-config.port = 4000 || process.env.PORT;
+config.port = process.env.PORT || 4000;
 
 if(process.env.NODE_ENV === 'development'){
   config.db_url = 'mongodb://127.0.0.1:27017/users';
